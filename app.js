b4297a052ede8650ede0477fb3beb066ead26dbd
Mike Frazier <michael.s.frazier@gmail.com>
2016-05-07 00:13:37 -0500

    Refactor GroceryListItem to be class component; add events and listeners

--- app.js
+++ app.js
@@ -1,25 +1,60 @@
 // TODO
-var GroceryList = () => (
-  // <ul>
-  //   <Garbage />
-  //   <Trash />
-  // </ul>
-  <GroceryListItem items={["Garbage", "Trash"]} />
-);
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      picked: false,
+      hovered: false
+    };
+  }
+  
+  onListItemClick () {
+    this.setState({
+      picked: !this.state.picked
+    });
+  }
+  
+  onListItemHover () {
+    this.setState({
+      hovered: !this.state.hovered
+    });
+  }
+  
+  render () {
+    var style = {
+      textDecoration: this.state.picked ? "line-through" : "none",
+      fontWeight: this.state.hovered ? "bold" : "normal"
+    };
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)} 
+        onMouseOver={this.onListItemHover.bind(this)} 
+        onMouseOut={this.onListItemHover.bind(this)} > {this.props.item} </li>
+    );
+  }
+}
 
-var Garbage = () => (
-  <li>Garbage</li>
-);
-
-var Trash = () => (
-  <li>Trash</li>
-);
-
-var GroceryListItem = (props) => (
+var GroceryList = (props) => (
   <ul>
-    <li> {props.items[0]} </li>
-    <li> {props.items[1]} </li>
+    { props.items.map( item => 
+      <GroceryListItem item={item} />
+    )}
   </ul>
-)
+);
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+ReactDOM.render(<GroceryList items={["Garbage","Trash","Stuff"]} />, document.getElementById("app"));
+
+
+// var Garbage = () => (
+//   <li>Garbage</li>
+// );
+
+// var Trash = () => (
+//   <li>Trash</li>
+// );
+
+// var GroceryListItem = (props) => (
+//   <ul>
+//     <li> {props.items[0]} </li>
+//     <li> {props.items[1]} </li>
+//   </ul>
+// )
