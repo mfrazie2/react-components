947032cc21a112726e41e9b8f316d745b154f72b
Mike Frazier <michael.s.frazier@gmail.com>
2016-05-06 23:19:47 -0500

    Begin building React components

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // TODO
+var GroceryList = () => (
+  <ul>
+    <Garbage />
+    <Trash />
+  </ul>
+);
+
+var Garbage = () => (
+  <li>Garbage</li>
+);
+
+var Trash = () => (
+  <li>Trash</li>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
